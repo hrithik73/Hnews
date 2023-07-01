bdbee8e657bdca0853ac43cdea8d39acf3fb5902
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-07-01 22:09:01 +0530

    feat: Project Init

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Button = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hritik</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Button;
