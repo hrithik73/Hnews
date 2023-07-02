8afa44a073ea9b945770c82e3fe157c3d4a2c8a9
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-07-02 14:43:41 +0530

    chore: folder restructure

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,7 @@
 import { memo, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { fetchData } from 'src/lib/fetchData';
-import { IStory } from 'types/story';
+import { IStory } from 'src/types/story';
 
 interface IListItem {
   storyId: string;
@@ -26,23 +26,28 @@ const ListItem = ({ storyId, index }: IListItem) => {
 
   return (
     <View style={styles.container}>
-      <Text>
-        {getIndex(index)}
-        <Text style={styles.title}> {storyData?.title}</Text> ({storyData?.url})
-      </Text>
-      <Text style={styles.subTitle}>
-        {storyData?.score} points by {storyData?.by} | {storyData?.kids?.length}{' '}
-        comments
-      </Text>
+      <Text>{getIndex(index)}</Text>
+      <View>
+        <Text>
+          <Text style={styles.title}> {storyData?.title}</Text>
+        </Text>
+        <Text style={styles.subTitle}>
+          {storyData?.score} points by
+          <Text style={styles.auther}> {storyData?.by} </Text> |{' '}
+          {storyData?.kids?.length} comments
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 50,
-    marginVertical: 10,
-    paddingHorizontal: 10,
+    flexDirection: 'row',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: '#1212',
   },
   title: {
     fontSize: 16,
@@ -50,9 +55,13 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   },
   subTitle: {
-    paddingTop: 5,
-    fontSize: 16,
-    paddingLeft: 20,
+    // paddingTop: 5,
+    paddingVertical: 5,
+    fontSize: 14,
+  },
+  auther: {
+    color: 'red',
+    margin: 10,
   },
 });
 
