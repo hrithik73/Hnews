2ee8cf2c6367982d93d9ece965416bbd6a79fe7a
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-07-02 15:06:50 +0530

    feat: added shimmer effect

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,18 +4,26 @@ import { fetchData } from 'src/lib/fetchData';
 import { IStory } from 'src/types/story';
 import { getIndex } from 'src/utils/helperFunctions';
 
+import { createShimmerPlaceholder } from 'react-native-shimmer-placeholder';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const ShimmerPlaceholder = createShimmerPlaceholder(LinearGradient);
+
 interface IListItem {
   storyId: string;
   index: number;
 }
 
 const ListItem = ({ storyId, index }: IListItem) => {
+  const [isFetched, setIsFetched] = useState(false);
   const [storyData, setStoryData] = useState<IStory>();
 
   useEffect(() => {
     const getStory = async () => {
+      setIsFetched(false);
       const res = await fetchData(`item/${storyId}`);
       setStoryData(res);
+      setIsFetched(true);
     };
 
     getStory();
@@ -23,24 +31,28 @@ const ListItem = ({ storyId, index }: IListItem) => {
 
   return (
     <View style={styles.container}>
-      <Text>{getIndex(index)}</Text>
-      <View>
-        <Text>
-          <Text style={styles.title}> {storyData?.title}</Text>
-        </Text>
-        <Text style={styles.subTitle}>
-          {storyData?.score} points by
-          <Text style={styles.auther}> {storyData?.by} </Text> |{' '}
-          {storyData?.kids?.length} comments
-        </Text>
-      </View>
+      <ShimmerPlaceholder visible={isFetched} height={70} width={500}>
+        <View style={styles.itemContainer}>
+          <Text>{getIndex(index)}</Text>
+          <View>
+            <Text>
+              <Text style={styles.title}> {storyData?.title}</Text>
+            </Text>
+            <Text style={styles.subTitle}>
+              {storyData?.score} points by
+              <Text style={styles.auther}> {storyData?.by} </Text> |{' '}
+              {storyData?.kids?.length} comments
+            </Text>
+          </View>
+        </View>
+      </ShimmerPlaceholder>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    height: 100,
     padding: 10,
     margin: 10,
     borderRadius: 10,
@@ -59,6 +71,9 @@ const styles = StyleSheet.create({
   auther: {
     color: 'red',
     margin: 10,
+  },
+  itemContainer: {
+    flexDirection: 'row',
   },
 });
 
