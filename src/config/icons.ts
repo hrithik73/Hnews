f41dd9bb15169ef2a76e445322e0f3868d25ea9d
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-07-03 10:54:47 +0530

    fix: changed icon name

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,5 +1,7 @@
 const icons = {
   filter: require('../assets/icons/filter.png'),
+  save: require('../assets/icons/save.png'),
+  save_outline: require('../assets/icons/save_outline.png'),
 };
 
 export default icons;
